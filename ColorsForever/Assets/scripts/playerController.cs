59233a3d6a7127217c6bbcd6ac1c1d5066ef4f96
unknown <Jonathan.Banashek@hol-itsys-dev.LYV.LiveNation.com>
2014-04-26 01:43:34 -0700

    Adding player controller and basic stage for testing

---
+++ ColorsForever/Assets/scripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask isGround;
+	public float jumpForce = 700f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, isGround);
+		float move = Input.GetAxis ("Horizontal");
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+	}
+
+	void Update() {
+		if(grounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+}
