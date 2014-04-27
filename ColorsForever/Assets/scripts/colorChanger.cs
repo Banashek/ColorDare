982c82c82554ad927e00de49e7dee501bac67c78
Jake Cohen <jake_cohen@me.com>
2014-04-26 19:32:38 -0700

    added color changer

---
+++ ColorsForever/Assets/scripts/colorChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorChanger : MonoBehaviour {
+
+	private colorPicker pickedColor;
+
+	// Use this for initialization
+	void Start () {
+		pickedColor = GameObject.FindWithTag("Spectrum").GetComponent<colorPicker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.color = pickedColor.PickedColor;
+	}
+}
