4eabc3106cebf1b071df9a38587f7f4b648a38e1
Evan Sullivan <darkfox3889@gmail.com>
2014-04-28 11:03:40 -0700

    Added SplashScreen PNG, Implemented ColorPicker and created GUIController object and script.

---
+++ ColorsForever/Assets/scripts/GUIController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+	public GUIStyle colorPicker;
+
+	// Update is called once per frame
+	void OnGUI () {
+	
+		GUI.Label (new Rect(0, 0, Screen.width, 65), "", colorPicker);
+
+	}
+}
